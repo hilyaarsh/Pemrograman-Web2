6661cea76dfda68ec3c5a1457d3ba6bc78e16336
hilyaarsh <rasihahilya@gmail.com>
2023-03-08 15:58:03 +0700

    Upload Praktikum 3

---
+++ Praktikum3/proses_registrasi.php
@@ -0,0 +1,19 @@
+<?php 
+$domisili = ["Jakarta", "Bogor", "Depok", "Tangerang", "Bekasi"];
+
+$program_studi = [
+    "SI"=>"Sistem Informasi",   // key=>value
+    "TI"=>"Teknik Informatika",
+    "BD"=>"Bisnis Digital"
+];
+
+$skills = [
+    "HTML"=>10,
+    "CSS"=>10,
+    "Javascript"=>20,
+    "PHP"=>30,
+    "Python"=>30,
+];
+
+
+?>
